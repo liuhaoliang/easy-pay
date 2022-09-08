ece72c18068e03a699dadaedd8c1a693857ca3f9
liuhaoliang <liuhaoliang@aliyun.com>
2022-09-09 05:47:41 +0800

    perf: 优化客户端连接管理

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const express = require("express");
-const sseClients = [];
-const send = (text)=>{
-  for (const sse of sseClients) {
-    sse.write("data: " + text + "\n\n");
-  }
-}
-const app = express();
-
 const path = require("path");
+const express = require("express");
+// 存储客户端连接
+const sseClients = new Set();
+// 发送sse消息
+const send = (text) => {
+  const clients = [...sseClients];
+  for (const client of clients) {
+    if (client.socket.destroyed) {
+      // 根据socket状态，删除无效客户端
+      sseClients.delete(client);
+    } else {
+      // 模拟sse发送格式
+      client.write("data: " + text + "\n\n");
+    }
+  }
+};
+const app = express();
 const port = 3030;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.get("/pay", (req, res) => {
-  send(req.query.extra)
+  send(req.query.extra);
   res.send("ok");
 });
 
 app.get("/sse", (req, res) => {
-  sseClients.push(res);
+  sseClients.add(res);
   res.header({
     "Content-Type": "text/event-stream",
     "Cache-Control": "no-cache",
